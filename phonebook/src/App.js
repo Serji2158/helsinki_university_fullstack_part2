514d7261f6417c89f74144120667e245d80c6bd8
Sergiy Tyschenko <sergey.tyschenko@gmail.com>
2023-03-12 18:23:59 +0200

    ex.2.11(axios,useeffect):completed

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ContactList from './ContactList'
 import Filter from './Filter'
 // import Input from './Input'
 import PersonForm from './PersonForm'
 import Title from './Title'
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  // const data = () => {
+  //   axios
+  //     .get('http://localhost:3001/persons')
+  //     .then(response => {
+  //       const personsData = response.data
+  //       setPersons(personsData)
+  //     })
+  // }
+
+  // useEffect(data, [])
+
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log('promise fulfilled:', response.data)
+        setPersons(response.data)
+      }
+    )
+  }, [])
 
   const isPersonExist = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
   const isNumberExist = persons.some(person => person.number === newNumber)
